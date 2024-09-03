1e8759cd781fe9e526d7c926486033234882ad38
Jawad Ali <jawadali000123@gmail.com>
2024-09-03 14:35:47 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import Home from "./components/home"
+
+export default function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen overflow-y-auto bg-blue-500">
+        Home
+    </div>
+  )
+}
